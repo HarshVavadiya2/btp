0a1dc2f1c85fe68b858fe047f287ea8769667bd6
SUSHIL KUMAR <115349532+mrsushilkumar@users.noreply.github.com>
2024-10-16 21:25:05 +0530

    random 50per dead block try 2

--- performance-analysis/mirage/src/cache.cc
+++ performance-analysis/mirage/src/cache.cc
@@ -3902,8 +3902,9 @@ void CACHE::remap_set_ceaser_s()
 		assume 50% block is dead randomly 
 			*/
 
-				srand(1551);
-				if (block[Sptr][way].isDead == (rand()%2))
+				srand(646148113179);
+				// if (block[Sptr][way].isDead == (rand()%2))
+				if ((rand()%100) <= 50)
 				{
 					/* code */
 					
